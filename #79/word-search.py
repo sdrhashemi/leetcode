8ada687d8334e1690e91d17d64fe87c9e3a384b2
sdrhashemi <sdrhashemi@gmail.com>
2025-04-19 13:34:43 +0330

    No #79, word search

---
+++ #79/word-search.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/word-search/
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        Rows, Cols = len(board), len(board[0])
+        path = set()
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (r<0 or c<0 or r>=Rows or c>=Cols or board[r][c] != word[i] or (r,c) in path):
+                return False
+            
+            path.add((r,c))
+            res = dfs(r-1,c,i+1) or dfs(r+1,c,i+1) or dfs(r,c-1,i+1) or dfs(r,c+1,i+1)
+            path.remove((r,c))
+            return res
+
+        for i in range(Rows):
+            for j in range(Cols):
+                if dfs(i, j, 0): return True
+        return False
