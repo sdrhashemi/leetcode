663550b27f7a43b079b19aaf87e9f1513337bc02
sdrhashemi <sdrhashemi@gmail.com>
2025-04-19 14:51:19 +0330

    Algo: merge and quick sorts

---
+++ Algorithms/quick-sort.py
@@ -0,0 +1,10 @@
+# Time:
+#     Average: O(n log n)
+#     Worst: O(n^2) (bad pivot)
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[0]
+    left  = [x for x in arr[1:] if x < pivot]
+    right = [x for x in arr[1:] if x >= pivot]
+    return quicksort(left) + [pivot] + quicksort(right)
